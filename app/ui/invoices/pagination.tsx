4dc76a5554038f3a03ff7b2ce438dead322dabb9
CheezyLeezy1 <x20115075@student.ncirl.ie>
2024-04-01 22:26:21 +0100

    Second Commit - Chapter 16 - Completed

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,11 +4,24 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: comment in this code when you get to this point in the course
+  // Here's a breakdown of what's happening:
+    // createPageURL creates an instance of the current search parameters.
+    // Then, it updates the "page" parameter to the provided page number.
+    // Finally, it constructs the full URL using the pathname and updated search parameters.
 
   // const allPages = generatePagination(currentPage, totalPages);
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathName}?${params.toString()}`;
+  };
 
   return (
     <>
